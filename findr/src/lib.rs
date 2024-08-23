43964bccb46b155f29965350b63d9d5e96738d11
mei30 <meisamjafarimosleh@gmail.com>
2024-08-23 16:06:31 +0330

    Pass tests

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -1,13 +1,60 @@
 use anyhow::{anyhow, Result};
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write};
+use clap::{builder::PossibleValue, ArgAction, Parser, ValueEnum};
+use regex::Regex;
+use walkdir::{DirEntry, WalkDir};
 
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
 /// Rust version of `find`
 struct Args {
+    /// Search path(s)
+    #[arg(value_name = "PATH", default_value = ".")]
+    paths: Vec<String>,
+
+    /// Names
+    #[arg(
+        short('n'),
+        long("name"),
+        value_name = "NAME",
+        value_parser(Regex::new),
+        action(ArgAction::Append),
+        num_args(0..)
+    )]
+    names: Vec<Regex>,
+
+    /// Entry types
+    #[arg(
+        short('t'),
+        long("type"),
+        value_name = "TYPE",
+        value_parser(clap::value_parser!(EntryType)),
+        action(ArgAction::Append),
+        num_args(0..)
+    )]
+    entry_types: Vec<EntryType>,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+impl ValueEnum for EntryType {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[EntryType::Dir, EntryType::File, EntryType::Link]
+    }
+
+    fn to_possible_value<'a>(&self) -> Option<PossibleValue> {
+        Some(match self {
+            EntryType::Dir => PossibleValue::new("d"),
+            EntryType::File => PossibleValue::new("f"),
+            EntryType::Link => PossibleValue::new("l"),
+        })
+    }
 }
 
 fn open(filename: &str) -> Result<Box<dyn BufRead>> {
@@ -19,6 +66,40 @@ fn open(filename: &str) -> Result<Box<dyn BufRead>> {
 
 pub fn run() -> Result<()> {
     let args = Args::parse();
+    let type_filter = |entry: &DirEntry| {
+        args.entry_types.is_empty()
+            || args.entry_types.iter().any(|entry_type| match entry_type {
+                EntryType::Link => entry.file_type().is_symlink(),
+                EntryType::Dir => entry.file_type().is_dir(),
+                EntryType::File => entry.file_type().is_file(),
+            })
+    };
+
+    let name_filter = |entry: &DirEntry| {
+        args.names.is_empty()
+            || args
+                .names
+                .iter()
+                .any(|re| re.is_match(&entry.file_name().to_string_lossy()))
+    };
+
+    for path in &args.paths {
+        let entries = WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| match e {
+                Err(e) => {
+                    eprintln!("{e}");
+                    None
+                }
+                Ok(entry) => Some(entry),
+            })
+            .filter(type_filter)
+            .filter(name_filter)
+            .map(|entry| entry.path().display().to_string())
+            .collect::<Vec<_>>();
+
+        println!("{}", entries.join("\n"));
+    }
 
     Ok(())
 }
